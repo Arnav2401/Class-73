cdca6b9294296bffbc6b639fada1f75d109591d7
Arnav2401 <77386547+Arnav2401@users.noreply.github.com>
2021-07-20 19:11:52 +0530

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import db from '../config';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default class Searchscreen extends React.Component {
+  
+  render() {
+    return (
+      <View >
+        <Text>This is the Second screen</Text>
+      </View>
+    );
+  }
+}
+
